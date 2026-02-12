4689d92847a0cab598c41dde93f16b41e450fb04
Aruna24799 <aruna04082005@gmail.com>
2026-02-12 21:41:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,62 +1,55 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-const supabase = createClient(
-"https://pxtpsugbuunjzurdvzkc.supabase.co",
-"sb_publishable_BOHqCxkzsVWChq-zAc4Q3Q_ED0khzHW"
-);
+const supabaseUrl = "YOUR_SUPABASE_URL";
+const supabaseKey = "YOUR_ANON_KEY";
 
-let currentEvent=null;
+const supabase = createClient(supabaseUrl, supabaseKey);
 
-window.showAdmin=()=>{
-role-screen.classList.add("hidden");
-admin-screen.classList.remove("hidden");
-loadAdmin();
-}
+window.showAdmin = () => {
+home.style.display="none";
+admin.style.display="block";
+loadEvents();
+};
 
-window.showParticipant=()=>{
-role-screen.classList.add("hidden");
-participant-screen.classList.remove("hidden");
-loadPublic();
-}
+window.showParticipant = () => {
+home.style.display="none";
+participant.style.display="block";
+loadEventsDropdown();
+};
 
-window.createEvent=async()=>{
-const name=eventName.value.trim();
-if(!name)return alert("Enter event");
+window.goHome = () => {
+location.reload();
+};
+
+window.createEvent = async () => {
+const name=document.getElementById("eventName").value;
 
 await supabase.from("events").insert([{name}]);
-eventName.value="";
-loadAdmin();
-}
 
-async function loadAdmin(){
-const {data}=await supabase.from("events").select("*").order("id",{ascending:false});
+loadEvents();
+};
+
+async function loadEvents(){
+const {data}=await supabase.from("events").select();
 events.innerHTML="";
 data.forEach(e=>{
-events.innerHTML+=`<div class="event">${e.name}</div>`;
+events.innerHTML+=`<p>${e.name}</p>`;
 });
 }
 
-async function loadPublic(){
-const {data}=await supabase.from("events").select("*");
-eventList.innerHTML="";
+async function loadEventsDropdown(){
+const {data}=await supabase.from("events").select();
+eventSelect.innerHTML="";
 data.forEach(e=>{
-eventList.innerHTML+=`<div class="event" onclick="pick(${e.id})">${e.name}</div>`;
+eventSelect.innerHTML+=`<option value="${e.id}">${e.name}</option>`;
 });
 }
 
-window.pick=id=>currentEvent=id;
+window.joinEvent = async ()=>{
+const name=document.getElementById("participantName").value;
+const event_id=document.getElementById("eventSelect").value;
 
-window.joinEvent=async()=>{
-if(!currentEvent)return alert("Select event");
-if(!pname.value||!pemail.value)return alert("Fill all");
+await supabase.from("participants").insert([{name,event_id}]);
 
-await supabase.from("participants").insert([{
-name:pname.value,
-email:pemail.value,
-event_id:currentEvent
-}]);
-
-alert("Joined!");
-pname.value="";
-pemail.value="";
-}
+alert("Joined Successfully");
+};
