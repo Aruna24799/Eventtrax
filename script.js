9e42c1751b99dc74eff036b10b1578808df813e7
Aruna24799 <aruna04082005@gmail.com>
2026-02-12 22:19:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,56 +1,73 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+const SUPABASE_URL = "sb_publishable_BOHqCxkzsVWChq-zAc4Q3Q_ED0khzHW";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB4dHBzdWdidXVuanp1cmR2emtjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA1NDY4OTIsImV4cCI6MjA4NjEyMjg5Mn0.VXRKe2AXSiv8vRxfoPDyBl9McRmkYDVUBcRN2Jy6q5g";
 
-const supabaseUrl = "https://pxtpsugbuunjzurdvzkc.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB4dHBzdWdidXVuanp1cmR2emtjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA1NDY4OTIsImV4cCI6MjA4NjEyMjg5Mn0.VXRKe2AXSiv8vRxfoPDyBl9McRmkYDVUBcRN2Jy6q5g";
+const headers = {
+"apikey": SUPABASE_KEY,
+"Authorization":"Bearer "+SUPABASE_KEY,
+"Content-Type":"application/json"
+};
 
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-window.showAdmin = () => {
-home.style.display="none";
-admin.style.display="block";
+function showAdmin(){
+rolePage.classList.add("hidden");
+adminPage.classList.remove("hidden");
 loadEvents();
-};
-
-window.showParticipant = () => {
-home.style.display="none";
-participant.style.display="block";
-loadEventsDropdown();
-};
-
-window.goHome = () => {
-location.reload();
-};
-
-window.createEvent = async () => {
-const name=document.getElementById("eventName").value;
-
-await supabase.from("events").insert([{name}]);
-
-loadEvents();
-};
-
-async function loadEvents(){
-const {data}=await supabase.from("events").select();
-events.innerHTML="";
-data.forEach(e=>{
-events.innerHTML+=`<p>${e.name}</p>`;
-});
 }
 
-async function loadEventsDropdown(){
-const {data}=await supabase.from("events").select();
+function showParticipant(){
+rolePage.classList.add("hidden");
+participantPage.classList.remove("hidden");
+loadEvents();
+
+document.getElementById("qr").innerHTML="";
+new QRCode(document.getElementById("qr"), window.location.href);
+}
+
+function goHome(){
+location.reload();
+}
+
+async function createEvent(){
+
+const name=document.getElementById("eventName").value;
+if(!name)return alert("Enter event");
+
+await fetch(`${SUPABASE_URL}/rest/v1/events`,{
+method:"POST",
+headers,
+body:JSON.stringify({name})
+});
+
+document.getElementById("eventName").value="";
+loadEvents();
+}
+
+async function loadEvents(){
+
+const res=await fetch(`${SUPABASE_URL}/rest/v1/events?select=*`,{headers});
+const data=await res.json();
+
+eventList.innerHTML="";
 eventSelect.innerHTML="";
+
 data.forEach(e=>{
+eventList.innerHTML+=`<div>${e.name}</div>`;
 eventSelect.innerHTML+=`<option value="${e.id}">${e.name}</option>`;
 });
 }
 
-window.joinEvent = async ()=>{
+async function joinEvent(){
+
 const name=document.getElementById("participantName").value;
 const event_id=document.getElementById("eventSelect").value;
 
-await supabase.from("participants").insert([{name,event_id}]);
+if(!name)return alert("Enter name");
 
-alert("Joined Successfully");
-};
+await fetch(`${SUPABASE_URL}/rest/v1/participants`,{
+method:"POST",
+headers,
+body:JSON.stringify({name,event_id})
+});
 
+alert("Joined!");
+participantName.value="";
+}
