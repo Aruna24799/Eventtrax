8c459fb6a9ffbc60d36f58f78ec32e1b1d3c0ac1
Aruna24799 <aruna04082005@gmail.com>
2026-02-13 13:46:14 +0530

    commit to github

--- script.js
+++ script.js
@@ -1,86 +1,58 @@
-const SUPABASE_URL="https://pxtpsugbuunjzurdvzkc.supabase.co";
-const SUPABASE_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB4dHBzdWdidXVuanp1cmR2emtjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA1NDY4OTIsImV4cCI6MjA4NjEyMjg5Mn0.VXRKe2AXSiv8vRxfoPDyBl9McRmkYDVUBcRN2Jy6q5g";
+const URL="https://pxtpsugbuunjzurdvzkc.supabase.co";
+const KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB4dHBzdWdidXVuanp1cmR2emtjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA1NDY4OTIsImV4cCI6MjA4NjEyMjg5Mn0.VXRKe2AXSiv8vRxfoPDyBl9McRmkYDVUBcRN2Jy6q5g";
 
 const headers={
-apikey:SUPABASE_KEY,
-Authorization:"Bearer "+SUPABASE_KEY,
+apikey:KEY,
+Authorization:`Bearer ${KEY}`,
 "Content-Type":"application/json"
 };
 
-function openAdmin(){
-landing.classList.add("hidden");
-dashboard.classList.remove("hidden");
-loadEvents();
-loadParticipants();
+function admin(){
+home.style.display="none";
+adminDiv.style.display="block";
+load();
 }
 
-function openParticipant(){
-landing.classList.add("hidden");
-participantPage.classList.remove("hidden");
-loadEvents();
-qr.innerHTML="";
-new QRCode(qr,location.href);
-}
-
-function logout(){location.reload()}
-
-function showSection(id){
-document.querySelectorAll(".section").forEach(s=>s.classList.add("hidden"));
-document.getElementById(id).classList.remove("hidden");
+function participant(){
+home.style.display="none";
+participantDiv.style.display="block";
+load();
 }
 
 async function createEvent(){
-const name=eventName.value;
-if(!name)return alert("Enter event");
+let name=eventName.value;
 
-await fetch(`${SUPABASE_URL}/rest/v1/events`,{
+await fetch(`${URL}/rest/v1/events`,{
 method:"POST",
 headers,
 body:JSON.stringify({name})
 });
 
-eventName.value="";
-loadEvents();
+load();
 }
 
-async function loadEvents(){
-const r=await fetch(`${SUPABASE_URL}/rest/v1/events?select=*`,{headers});
-const data=await r.json();
+async function load(){
+let r=await fetch(`${URL}/rest/v1/events`,{headers});
+let d=await r.json();
 
-eventList.innerHTML="";
+events.innerHTML="";
 eventSelect.innerHTML="";
-totalEvents.innerText=data.length;
 
-data.forEach(e=>{
-eventList.innerHTML+=`<div class="event">${e.name}</div>`;
+d.forEach(e=>{
+events.innerHTML+=`<p>${e.name}</p>`;
 eventSelect.innerHTML+=`<option value="${e.id}">${e.name}</option>`;
 });
 }
 
-async function joinEvent(){
-const name=participantName.value;
-const event_id=eventSelect.value;
-
-await fetch(`${SUPABASE_URL}/rest/v1/participants`,{
+async function join(){
+await fetch(`${URL}/rest/v1/participants`,{
 method:"POST",
 headers,
-body:JSON.stringify({name,event_id})
+body:JSON.stringify({
+name:userName.value,
+event_id:eventSelect.value
+})
 });
 
 alert("Joined");
 }
-
-async function loadParticipants(){
-const r=await fetch(`${SUPABASE_URL}/rest/v1/participants?select=*`,{headers});
-const data=await r.json();
-
-participantList.innerHTML="";
-totalParticipants.innerText=data.length;
-
-data.forEach(p=>{
-participantList.innerHTML+=`<div class="event">${p.name}</div>`;
-});
-}
-
-
-
