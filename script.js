4c57ee52043453b68c5f7926e802381f155d5067
Aruna24799 <aruna04082005@gmail.com>
2026-02-13 14:46:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 const SUPABASE_URL = "https://pxtpsugbuunjzurdvzkc.supabase.co";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB4dHBzdWdidXVuanp1cmR2emtjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA1NDY4OTIsImV4cCI6MjA4NjEyMjg5Mn0.VXRKe2AXSiv8vRxfoPDyBl9McRmkYDVUBcRN2Jy6q5g";
-
 const headers = {
 apikey: SUPABASE_KEY,
 Authorization: "Bearer " + SUPABASE_KEY,
-"Content-Type": "application/json"
+"Content-Type":"application/json"
 };
 
 const home=document.getElementById("home");
 const admin=document.getElementById("admin");
 const participant=document.getElementById("participant");
-const events=document.getElementById("events");
+const eventsDiv=document.getElementById("events");
 const eventSelect=document.getElementById("eventSelect");
 
 function openAdmin(){
@@ -25,32 +24,44 @@ participant.classList.remove("hidden");
 loadEvents();
 }
 
+function goHome(){
+location.reload();
+}
+
 async function createEvent(){
 const name=document.getElementById("eventName").value;
+if(!name)return alert("Enter event");
+
 await fetch(`${SUPABASE_URL}/rest/v1/events`,{
 method:"POST",
 headers,
 body:JSON.stringify({name})
 });
+
+document.getElementById("eventName").value="";
 loadEvents();
 }
 
 async function loadEvents(){
+
 const res=await fetch(`${SUPABASE_URL}/rest/v1/events?select=*`,{headers});
 const data=await res.json();
 
-events.innerHTML="";
+eventsDiv.innerHTML="";
 eventSelect.innerHTML="";
 
 data.forEach(e=>{
-events.innerHTML+=`<p>${e.name}</p>`;
+eventsDiv.innerHTML+=`<div>${e.name}</div>`;
 eventSelect.innerHTML+=`<option value="${e.id}">${e.name}</option>`;
 });
 }
 
 async function joinEvent(){
+
 const name=document.getElementById("userName").value;
 const event_id=eventSelect.value;
+
+if(!name)return alert("Enter name");
 
 await fetch(`${SUPABASE_URL}/rest/v1/participants`,{
 method:"POST",
@@ -59,4 +70,6 @@ body:JSON.stringify({name,event_id})
 });
 
 alert("Joined!");
+document.getElementById("userName").value="";
 }
+
