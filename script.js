c7a5c0dc8499908735ca74c2c865a5c7dc1fcf32
Aruna24799 <aruna04082005@gmail.com>
2026-02-17 00:11:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,13 +5,38 @@ const client = createClient(
 "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB4dHBzdWdidXVuanp1cmR2emtjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA1NDY4OTIsImV4cCI6MjA4NjEyMjg5Mn0.VXRKe2AXSiv8vRxfoPDyBl9McRmkYDVUBcRN2Jy6q5g"
 );
 
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+
 document.getElementById("loginBtn").addEventListener("click", login);
 document.getElementById("signupBtn").addEventListener("click", signup);
 
-function signup(){
-alert("signup clicked");
+async function signup(){
+
+const { error } = await client.auth.signUp({
+email: email.value,
+password: password.value
+});
+
+if(error){
+alert(error.message);
+}else{
+alert("Signup success — check email");
 }
 
-function login(){
-alert("login clicked");
+}
+
+async function login(){
+
+const { data, error } = await client.auth.signInWithPassword({
+email: email.value,
+password: password.value
+});
+
+if(error){
+alert(error.message);
+}else{
+alert("Login success");
+}
+
 }
